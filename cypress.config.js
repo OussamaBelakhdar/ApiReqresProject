3b7d9464e9bd5982b713e5eac5c814e518bee8ec
Oussama Belakdhar <97692138+OussamaBelakhdar@users.noreply.github.com>
2023-08-17 12:50:54 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,12 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      baseUrl: "http://localhost:3000",
-        integrationFolder: "cypress/e2e",
-        testFiles: "**/*.cy.js",
-        viewportWidth: 1280,
-        viewportHeight: 720,
-        video: true
-  },
+      // implement node event listeners here
+    },
   },
 });
